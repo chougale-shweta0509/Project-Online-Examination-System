afb5efe11556db0389e5e149bbfabf02902461a7
chougale-shweta0509 <shwetachougale8571@gmail.com>
2024-08-15 15:53:41 +0530

    Data,Service,ViewModel,Web,BLL

---
+++ Examination.BLL/Services/IAccountService.cs
@@ -0,0 +1,17 @@
+﻿using Examination.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Examination.BLL.Services
+{
+    public interface IAccountService
+    {
+
+        bool AddTeacher(UserViewModel userViewModel);
+
+
+    }
+}
